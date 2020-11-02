dfe0f707516bb1dfc7cb19935ceea391024da327
Samuel Einheri <Samuel.einheri@gmail.com>
2020-11-02 10:15:48 +0100

    Added comments with references

--- Assets/Code/Scripts/RadialMenu.cs
+++ Assets/Code/Scripts/RadialMenu.cs
@@ -2,17 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RadialMenu : MonoBehaviour
+namespace SE
 {
-    // Start is called before the first frame update
-    void Start()
+    #region REFERENCES
+    // How to make a circular (pie) menu part 1
+    // https://www.youtube.com/watch?v=qBsLezkjJck
+    // How to make a circular (pie) menu part 2
+    // https://www.youtube.com/watch?v=EqKVS2wEl-w
+    // Make a Ring menu in Unity
+    // https://www.youtube.com/watch?v=tPH_BYRsvxI
+    // Make a Raidal Menu in Unity
+    // https://www.youtube.com/watch?v=l_1NLtf6c0I
+    #endregion
+    
+    public class RadialMenu : MonoBehaviour
     {
-        
-    }
+        // Start is called before the first frame update
+        void Start()
+        {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
     }
 }
